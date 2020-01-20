5f531e9cc063cdcf7b7f2bdd4c2c873f749db71b
Jan Krapac <jan.krapac@gmail.com>
2020-01-20 23:30:58 +0100

    Sanize dependencies

--- src/__tests__/MonthSelect.test.tsx
+++ src/__tests__/MonthSelect.test.tsx
@@ -5,4 +5,5 @@ import MonthSelect from '../components/MonthSelect';
 it('renders a month selector', () => {
     const wrapped = shallow(<MonthSelect />);
     expect(wrapped.find('select')).toHaveLength(1);
+    expect(wrapped.find('select').children()).toHaveLength(12);
 });
